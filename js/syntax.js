8c5ee9ccfc7fe34013260d740fe68e27cba4cd5b
ViNi89 <vitsas@aueb.gr>
2017-02-04 19:43:55 +0200

    Initial commit

---
+++ js/syntax.js
@@ -0,0 +1,30 @@
+function nox_blog_init() {
+/*
+   var Snippets = document.getElementsByTagName("nox-code");
+   for(var Index = 0; Index < Snippets.length; Index++) 
+   {
+//       alert(Snippets[Index].innerHTML.split(/\w+/g));
+   }
+*/
+//    alert(nxDKeywords["int"]); 
+/*  hljs.initHighlightingOnLoad()
+  var Snippets = document.getElementsByTagName("nox-code");
+  for(var Index = 0; Index < Snippets.length; Index++) 
+    {
+//	alert("a");
+	alert(Snippets[Index]);
+        hljs.highlightBlock(Snippets[Index]);
+//	alert("b");
+  
+    }
+
+//  $('nox-code').each(function(i, block) {
+//    hljs.highlightBlock(block);
+//  });
+*/
+}
+
+nxDKeywords = {
+    "struct": 0,
+    "int": 0
+}
